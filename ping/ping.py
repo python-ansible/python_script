93476b4d48b8c3b4c167aa6b64bd06f72e197a82
王泽霖 <python-ansible@users.noreply.github.com>
2017-05-27 10:54:04 +0800

    fix bug

--- ping/ping.py
+++ ping/ping.py
@@ -39,11 +39,13 @@ if __name__ == "__main__":
     cp = ConfigParser.SafeConfigParser()
     cp.read('./hosts.txt')
     ip = cp.get('ip','ip').split(',')
+    #线程数，默认2，最好不超过核心数
     pool=multiprocessing.Pool(processes=2)
     result=[]
     print "#########################%s#########################"%now
     print "IPADDRSS\t\tLOSS\t\tMIN\t\tMAX\t\tAVG"
     for i in ip:
-        result.append(pool.apply_async(pinger,(10,i))) 
+    #ping操作的次数，默认3
+        result.append(pool.apply_async(pinger,(3,i))) 
     pool.close()           
     pool.join()
